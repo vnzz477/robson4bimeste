1b36f4ff07f105f2d094c20755ed977185140e55
Vinicius Freire Costa <ra54865444807@cursos.insf>
2025-11-14 09:46:58 -0300

    subindo arquivos

--- data/create.sql
+++ data/create.sql
@@ -9,3 +9,13 @@ nome varchar(200),
 email varchar(200),
 senha varchar(200)
 );
+
+create table tb_admin (
+  id_admin int primary key auto_increment,
+  nome varchar(200),
+  email varchar(200),
+  senha varchar(200)
+);
+
+
+select * from tb_usuario;
